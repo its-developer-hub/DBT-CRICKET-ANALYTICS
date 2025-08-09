efb3c0156dfb70ea4686636dce3ff6faf735a221
Velavan Dhanavel <getitrajesh@rediffmail.com>
2025-08-09 10:44:32 +0000

    updated

--- models/cricket/tournaments_view.sql
+++ models/cricket/tournaments_view.sql
@@ -1,20 +1,16 @@
--- Read data from Tournaments_view to Tournaments table
+{{ config(materialized='table') }}
 
-{{ config(
-          materialized='view',
-          database='dev_data_lake',
-          schema='raw_stage_cricket'
-          ) }}
+with source_data as (
 
-SELECT  value:id::string AS Tournament_ID,
-        value:name::string AS Tournament_Name,
-        value:sport:name::string AS Sports_Name,
-        value:type::string AS Tournament_type,
-        value:category:name::string AS Tournament_Category,
-        value:gender::string AS Tournament_Gender,
-        value:current_season:year::string AS Year,
-        value:current_season:name::string AS Current_Season_Name,
-        value:current_season:start_date::string AS Season_Start_Date,
-        value:current_season:end_date::string AS Season_End_Date
-FROM {{ source('cricket','tournaments') }} t,
-   lateral flatten ( input => json_data:tournaments ) f;
+    select 1 as id, 'A1' as val
+    union all
+    select 2 as id, 'A2' as val
+    union all
+    select 3 as id, 'A3' as val
+    union all
+    select -1 as id, null as val
+
+)
+
+select *
+from source_data
